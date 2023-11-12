6f636575acc708083542304d4f9f6a5281b29a02
VISHWANATH <W9611172@live.tees.ac.uk>
2023-11-12 18:17:08 +0000

    login/signup screens

--- app/src/main/java/com/example/travlog/MainActivity.kt
+++ app/src/main/java/com/example/travlog/MainActivity.kt
@@ -7,37 +7,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.travlog.login.LoginViewModel
 import com.example.travlog.ui.theme.TraVlogTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //FirebaseApp.initializeApp(this)
         setContent {
+            val loginViewModel = viewModel(modelClass = LoginViewModel::class.java)
             TraVlogTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Navigation(loginViewModel = loginViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TraVlogTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/travlog/Navigation.kt
@@ -0,0 +1,72 @@
+package com.example.travlog
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.example.travlog.home.Home
+import com.example.travlog.login.LoginScreen
+import com.example.travlog.login.LoginViewModel
+import com.example.travlog.login.SignUpScreen
+
+enum class LoginRoutes{
+    SignUp,
+    SignIn
+}
+
+enum class HomeRoutes{
+    Home,
+    Detail
+}
+
+@Composable
+fun Navigation(
+    navController: NavHostController = rememberNavController(),
+    loginViewModel: LoginViewModel
+){
+    NavHost(
+        navController = navController,
+        startDestination = LoginRoutes.SignIn.name
+    ){
+        composable(route = LoginRoutes.SignIn.name){
+            LoginScreen(onNavToHomepage = {
+                navController.navigate(HomeRoutes.Home.name){
+                    launchSingleTop = true
+                    popUpTo(route = LoginRoutes.SignIn.name){
+                        inclusive = true
+                    }
+                }
+            },
+                loginViewModel = loginViewModel
+                ) {
+                navController.navigate(LoginRoutes.SignUp.name){
+                    launchSingleTop = true
+                    popUpTo(LoginRoutes.SignIn.name){
+                        inclusive = true
+                    }
+                }
+            }
+        }
+
+        composable(route = LoginRoutes.SignUp.name){
+            SignUpScreen(onNavToHomepage = {
+                navController.navigate(HomeRoutes.Home.name){
+                    popUpTo(LoginRoutes.SignUp.name){
+                        inclusive = true
+                    }
+                }
+            },
+                loginViewModel = loginViewModel
+                ) {
+                navController.navigate(LoginRoutes.SignIn.name)
+            }
+        }
+
+        composable(route = HomeRoutes.Home.name){
+            Home()
+        }
+    }
+
+}
